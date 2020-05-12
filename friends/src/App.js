5c5e036bc518c21bfff3213314e446cf13d82417
masantea <mercyansong@gmail.com>
2020-05-12 00:41:14 -0400

    initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Login from "./components/Login"
+import GasPrices from "./components/GasPrices";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}}
+
+export default App;
